3d374aa59b022dd0abf80d8c34472d709c7077db
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2025-07-19 17:31:02 +0530

    potgress and endpoints done and tested

--- app/app/api/streams/route.ts
+++ app/app/api/streams/route.ts
@@ -1,13 +1,14 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import { Stream } from '../../generated/prisma/index';
+import youtubesearchapi from "youtube-search-api";
 
-const YT_REGEX= new RegExp("^https:\/\/www\.youtube\.com\/watch\?v=[\w-]{11}$")
+const YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
+
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
     url: z.string().refine(
-        (val) => val.toLocaleLowerCase().includes("spotify") || val.includes("youtube"),
+        (val) => val.includes("spotify") || val.includes("youtube"),
         "Url should be of spotify or youtube "
     )
 })
@@ -17,7 +18,8 @@ export async function POST(req: NextRequest) {
     try {
 
         const data = CreateStreamSchema.parse(await req.json());
-        const isYt = YT_REGEX.test(data.url)
+        const isYt = data.url.match(YT_REGEX)
+     
         if (!isYt) {
             return NextResponse.json({
             message: "Wrong url format"
@@ -26,20 +28,33 @@ export async function POST(req: NextRequest) {
             })
             
         }
-        const extractedId= data.url.split("?v=")[1]
+        const extractedId = data.url.split("?v=")[1];
 
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+        console.log(res.title)
+        const thumbnails =res.thumbnail.thumbnails
+        thumbnails.sort((a :{width:number},b:{width:number})=> a.width < b.width ? -1: 1)
 
-     await  prismaClient.stream.create({
-            data: {
-                userId: data.creatorId,
-                url: data.url,
-                extractedId,
-                type: "Youtube"
+  const stream =  await  prismaClient.stream.create({
+      data: {
+          userId: data.creatorId,
+          url: data.url,
+          extractedId,
+          type: "Youtube",
+          title: res.title ?? "Can't Find Video ",
+          smallImg: thumbnails.length >1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url ?? "https://images.app.goo.gl/r9Qo6pnSwoovgbJg9",
+          bigImg: thumbnails[thumbnails.length - 1].url ?? "https://images.app.goo.gl/r9Qo6pnSwoovgbJg9",
+          
             }
 
+     })
+        return NextResponse.json({
+            message: "Added stream",
+            id: stream.id
         })
 
     } catch (e) {
+        console.error("stream err ",e)
         return NextResponse.json({
             message: "Error while adding a stream"
         }, {
@@ -61,3 +76,5 @@ export async function GET(req:NextRequest) {
         streams
     })
 }
+
+
